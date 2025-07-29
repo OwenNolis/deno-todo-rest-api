79e117a5a025a7203570ce6f041f9daf7b242787
OwenNolis <83425846+OwenNolis@users.noreply.github.com>
2025-07-29 13:29:16 +0200

    Create update and delete route

--- controllers/todosController.ts
+++ controllers/todosController.ts
@@ -1,4 +1,4 @@
-import { Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { RouterContext } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { Todo } from "../models/todo.ts";
 
 let todos: Todo[] = [
@@ -6,11 +6,11 @@ let todos: Todo[] = [
   { id: "2", text: "Maak demo project" },
 ];
 
-export const getTodos = (ctx: Context) => {
+export const getTodos = (ctx: RouterContext<"/todos">) => {
   ctx.response.body = { todos };
 };
 
-export const addTodo = async (ctx: Context) => {
+export const addTodo = async (ctx: RouterContext<"/todos">) => {
   const body = await ctx.request.body({ type: "json" }).value;
   const newTodo: Todo = {
     id: crypto.randomUUID(),
@@ -19,4 +19,30 @@ export const addTodo = async (ctx: Context) => {
   todos.push(newTodo);
   ctx.response.status = 201;
   ctx.response.body = { message: "Todo toegevoegd", todo: newTodo };
+};
+
+export const deleteTodo = (ctx: RouterContext<"/todos/:id">) => {
+  const id = ctx.params.id;
+  todos = todos.filter((todo) => todo.id !== id);
+  ctx.response.body = { message: `Todo met id ${id} verwijderd` };
+};
+
+export const updateTodo = async (ctx: RouterContext<"/todos/:id">) => {
+  const id = ctx.params.id;
+  const body = await ctx.request.body({ type: "json" }).value;
+
+  const todo = todos.find((t) => t.id === id);
+
+  if (!todo) {
+    ctx.response.status = 404;
+    ctx.response.body = { message: "Todo niet gevonden" };
+    return;
+  }
+
+  todo.text = body.text ?? todo.text;
+
+  ctx.response.body = {
+    message: `Todo met id ${id} geüpdatet`,
+    todo,
+  };
 };
--- routes/todos.ts
+++ routes/todos.ts
@@ -1,10 +1,12 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import { getTodos, addTodo } from "../controllers/todosController.ts";
+import { getTodos, addTodo, deleteTodo, updateTodo } from "../controllers/todosController.ts";
 
 const router = new Router();
 
 router
   .get("/todos", getTodos)
-  .post("/todos", addTodo);
+  .post("/todos", addTodo)
+  .delete("/todos/:id", deleteTodo)
+  .put("/todos/:id", updateTodo);
 
 export default router;
